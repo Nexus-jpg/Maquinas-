3ee304025666987c8958ed00b6aa9e5a85e198d6
Gabriellucas172402 <gabriellucas17062010p.l@gmail.com>
2026-08-20 09:22:06 -0300

    iniciando_projeto01
    Added a user registration form with username and password fields.

--- Tela_cadastro.php
+++ Tela_cadastro.php
@@ -1,11 +1,31 @@
+<?php
+
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>cadastro</title>
 </head>
 <body>
-    
+    <form action="/submit-data" method="POST">
+  <div>
+    <label for="username">usuario:</label>
+    <input type="text" id="username" name="username" required>
+  </div>
+  
+  <div>
+    <label for="password">senha:</label>
+    <input type="password" id="password" name="password" required>
+  </div>
+  
+  <button type="submit">Logar</button>
+</form>
+
 </body>
 </html>
