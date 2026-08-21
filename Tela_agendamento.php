8b958ff68ebea12d12ebf448ba28457f7163a4a2
Gabriellucas172402 <gabriellucas17062010p.l@gmail.com>
2026-08-21 09:21:18 -0300

    Update Tela_agendamento.php with CSS and heading
    Added a heading to the body and linked a CSS file.

--- Tela_agendamento.php
+++ Tela_agendamento.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="css/Tela_agendamento.css">
     
 </head>
 <body>
-    
+     <h1>Nossos maquinarios</h1>
 </body>
 </html>
