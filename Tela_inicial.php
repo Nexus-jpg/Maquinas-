82020f23e650b6bb43cad3a1cfb02c315617ca52
Gabriellucas172402 <gabriellucas17062010p.l@gmail.com>
2026-08-21 09:29:24 -0300

    Refactor Tela_inicial.php for English language support
    Updated HTML structure and language from Portuguese to English.

--- Tela_inicial.php
+++ Tela_inicial.php
@@ -5,17 +5,13 @@
 
 
 ?>
-
-
-
-
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="css/Tela_inicial.css">
     
 </head>
 <body>
