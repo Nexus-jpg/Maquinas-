2c988f1ffeae15408ec6bb0c788c0c8578c89883
Gabriellucas172402 <gabriellucas17062010p.l@gmail.com>
2026-08-21 12:59:11 -0300

    Update Tela_inicial.php

--- Tela_inicial.php
+++ Tela_inicial.php
@@ -10,11 +10,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>primeira página</title>
     <link rel="stylesheet" href="css/Tela_inicial.css">
     
 </head>
 <body>
-    
+
+<div class="titulo">
+Bem vindo a nossa empresa.
+
+</div>
+
+
+ <div class="maquinarios_subtitulo">Nossos maquinarios</div>
+  <img src="" alt="">
+
+
+ 
+
+
+
 </body>
 </html>
