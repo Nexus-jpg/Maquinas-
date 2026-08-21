cad8a2b56fef72019b6f3f480ea43c539ffcad20
Gabriellucas172402 <gabriellucas17062010p.l@gmail.com>
2026-08-21 09:11:56 -0300

    Update Tela_pedidos.php
    Added a heading and linked a CSS file for styling.

--- Tela_pedidos.php
+++ Tela_pedidos.php
@@ -4,10 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="css/Tela_pedidos.css">
     
 </head>
 <body>
+    <h1>Agendamento</h1>
+    <h2>Pedidos:</h2>
     
+     
+
+
 </body>
 </html>
