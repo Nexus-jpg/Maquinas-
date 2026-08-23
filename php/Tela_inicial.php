7649731e85b9c4cb13a65bc2df71ac2024d2076a
Gabriellucas172402 <gabriellucas17062010p.l@gmail.com>
2026-08-23 17:59:21 -0300

    Add search form to Tela_inicial.php
    Added a search form to Tela_inicial.php for machinery lookup.

--- php/Tela_inicial.php
+++ php/Tela_inicial.php
@@ -1,6 +1,8 @@
 <?php
 
 
+require_once '../conector/conexao.php';
+
 
 
 
@@ -24,7 +26,11 @@ Bem vindo a nossa empresa.
 
  <div class="maquinarios_subtitulo">Nossos maquinarios</div>
   <img src="" alt="">
-
+    
+<form action="Tela_inicial.php" method="GET">
+    <input type="text" name="busca" placeholder="Digite o maquinário...">
+    <button type="submit">Buscar</button>
+</form>
 
  
 
