0059e0013e492182a5d2c53e07f40c037610fb8d
Gabriellucas172402 <gabriellucas17062010p.l@gmail.com>
2026-08-24 10:24:51 -0300

    Add basic HTML structure to Tela_sabe_mais.php

--- php/Tela_sabe_mais.php
+++ php/Tela_sabe_mais.php
@@ -1,1 +1,17 @@
+<?php
 
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sabe mais</title>
+</head>
+<body>
+    <h1>Sobre nós: </h1>
+</body>
+</html>
