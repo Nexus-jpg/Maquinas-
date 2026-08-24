c566537d740fefbf7b04a55f23a066eba4338fb4
Gabriellucas172402 <gabriellucas17062010p.l@gmail.com>
2026-08-24 10:36:32 -0300

    Update Tela_agendamento.php

--- php/Tela_agendamento.php
+++ php/Tela_agendamento.php
@@ -17,5 +17,32 @@
 </head>
 <body>
      <h1>Nossos maquinarios</h1>
+        <header class="head">
+        <nav class="navbar">
+
+            <a href="Tela_inicial.php">Inicio</a>
+            <a href="Tela_produtos.php">Maquinarios</a>
+            <a href="Tela_pedidos.php">pedidos</a>
+            <a href="Tela_cadastro.php">cadastro</a>
+            <a href="">sobre nós</a>
+        </nav>
+        <form action="" class="search-bar">
+            <input type="text" placeholder="Pesquisa...">
+            <button type="submit">
+
+            </button>
+        </form>
+
+
+
+
+
+    </header>
+
+
+    <script src="Tela_cadastro.js"></script>
+
+
+    
 </body>
 </html>
