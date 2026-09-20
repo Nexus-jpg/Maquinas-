55273dcb18b6ee62c5133bcbcd385771f3790945
Gabriellucas172402 <gabriellucas17062010p.l@gmail.com>
2026-09-20 12:51:20 -0300

    Update Tela_inicial.php

--- php/Usuário/Tela_inicial.php
+++ php/Usuário/Tela_inicial.php
@@ -24,7 +24,8 @@ $maquinas = $stmt->fetchAll(PDO::FETCH_ASSOC);
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>HeavyRent - Início</title
-  <link rel="stylesheet" href="../css/Tela_inicial.css">                             
+       <link rel="stylesheet" href="../../css/Usuário_css/Tela_inicial.css">
+                          
   <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@700&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
 </head>
 <body>
